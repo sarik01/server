59966b3fb9e19bdf469574551b8a1df371b77709
sarva <sarvar_kamilov2@mail.ru>
2023-05-10 19:15:12 -0800

    updates

--- client.py
+++ client.py
@@ -1,29 +1,33 @@
 import socket
 import json
 
-HOST = ('localhost', 10000)
-try:
-    # endpoint AF_INET -> ipv4, SOCK_STREAM -> TCP responses for port
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(HOST)
-    print('Connected to ', HOST)
+def client_start():
+    HOST = ('localhost', 10000)
+    try:
+        # endpoint AF_INET -> ipv4, SOCK_STREAM -> TCP responses for port
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(HOST)
+        print('Connected to ', HOST)
 
-    request = f"\r\nHTTP/1.1 200 OK".encode('utf-8')
-    request_byte = bytes(request)
+        request = f"\r\nHTTP/1.1 200 OK".encode('utf-8')
+        request_byte = bytes(request)
 
-    client.sendall(request_byte)
-    print('Sent msg!')
+        client.sendall(request_byte)
+        print('Sent msg!')
 
-    msg = ''
-    while 1:
+        msg = ''
+        while 1:
 
-        data = client.recv(8)
-        msg += data.decode('UTF-8')
-        if not len(data):
-            break
+            data = client.recv(8)
+            msg += data.decode('UTF-8')
+            if not len(data):
+                break
 
-    json_load = json.loads(msg)
-    print(json_load)
-except ConnectionError:
-    print('Connection is Interrupted!')
+        json_load = json.loads(msg)
+        print(json_load)
+    except ConnectionError:
+        print('Connection is Interrupted!')
 
+
+if __name__ == '__main__':
+    client_start()
--- server.py
+++ server.py
@@ -39,54 +39,60 @@ def get_data(addr: tuple) -> str:
     return text
 
 
-# gethostname -> localhost, port = 10000
-HOST = ('localhost', 10000)
+def start_server():
+    # gethostname -> localhost, port = 10000
+    HOST = ('localhost', 10000)
+    NUMBER_OF_CLIENTS = 4
 
-# endpoint AF_INET -> ipv4, SOCK_STREAM -> TCP responses for port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # endpoint AF_INET -> ipv4, SOCK_STREAM -> TCP responses for port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Avoid timeout
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Avoid timeout
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Bind!
-s.bind(HOST)
+    # Bind!
+    s.bind(HOST)
 
-# Listen incoming TCP connections
-s.listen()
-print('I am listening your connections!')
+    # Listen incoming TCP connections
+    s.listen(NUMBER_OF_CLIENTS)
+    print('I am listening your connections!')
 
-# Waiting for connections!
-while 1:
-    # accept() is to accept connection from client
-    # conn -> client Socket
-    # addr ->  Address of client
-    conn, addr = s.accept()
-    print('Connected-', addr)
-    data = conn.recv(1024)
-    print(data.decode('utf-8'))
+    # Waiting for connections!
+    while 1:
+        # accept() is to accept connection from client
+        # conn -> client Socket
+        # addr ->  Address of client
+        conn, addr = s.accept()
+        print('Connected-', addr)
+        data = conn.recv(1024)
+        print(data.decode('utf-8'))
 
-    user = cursor.execute(f'SELECT * FROM user WHERE address = "{addr[0]}"')
-    if user.fetchone():
-        choice = random.choice([15, 30, 60])
-        timer = time.sleep(choice)
-        data = get_data(addr)
-        res = json.dumps(data)
-        conn.send(res.encode('UTF-8'))
+        user = cursor.execute(f'SELECT * FROM user WHERE address = "{addr[0]}"')
+        if user.fetchone():
+            choice = random.choice([15, 30, 60])
+            timer = time.sleep(choice)
+            data = get_data(addr)
+            res = json.dumps(data)
+            conn.send(res.encode('UTF-8'))
+            conn.close()
+            print('Waiting for request!')
+
+        else:
+            gen_num = generate_alphanum_random_string(10)
+            request = f"\r\nHTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: {len(gen_num)}\r\nConnection: close"
+            text = f'{request}\r\n\r\n["{gen_num}"]'
+            insert_data(addr[0], gen_num)
+
+            res = json.dumps(text)
+            # send msg to client
+            conn.send(res.encode('UTF-8'))
+            conn.close()
+
+            print('Waiting for request!')
+
+        # # close connection
         conn.close()
-        print('Waiting for request!')
 
-    else:
-        gen_num = generate_alphanum_random_string(10)
-        request = f"\r\nHTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: {len(gen_num)}\r\nConnection: close"
-        text = f'{request}\r\n\r\n["{gen_num}"]'
-        insert_data(addr[0], gen_num)
 
-        res = json.dumps(text)
-        # send msg to client
-        conn.send(res.encode('UTF-8'))
-        conn.close()
-
-        print('Waiting for request!')
-
-    # # close connection
-    conn.close()
+if __name__ == '__main__':
+    start_server()
